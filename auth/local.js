43eab1a99c9fffe4ff258e31733c600fe447959f
Nicolas <ntraut9@gmail.com>
2022-02-19 18:03:10 +0100

    fix localSignup hanging problem

--- auth/local.js
+++ auth/local.js
@@ -5,7 +5,7 @@ const saltRounds = 10;
 
 /** @todo Fix https://github.com/neuroanatomy/NeuroWebLab/issues/1 */
 
-module.exports = ({app, usernameField}) => {
+module.exports = ({ app, usernameField }) => {
 
   passport.use(new LocalStrategy(
     (username, password, done) => {
@@ -31,7 +31,7 @@ module.exports = ({app, usernameField}) => {
   app.post(
     '/localLogin',
     passport.authenticate('local', {
-      failureRedirect : '/html/localsignin.html'
+      failureRedirect: '/html/localsignin.html'
     }),
     (req, res) => {
       res.redirect(301, req.session.returnTo || '/');
@@ -42,18 +42,18 @@ module.exports = ({app, usernameField}) => {
 
     /* bcrypt more secrure */
     bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
-      if(err) { res.status(500).send(err); } else {
+      if (err) { res.status(500).send(err); } else {
 
         /* save req.body.username & hash */
         const user = {
-          strategy : 'local',
+          strategy: 'local',
           // username : req.body.username,
-          passhash : hash
+          passhash: hash
         };
         user[usernameField] = req.body.username;
 
         app.db.addUser(user);
-        res.status(200);
+        res.status(200).send();
       }
     });
   });
@@ -62,7 +62,7 @@ module.exports = ({app, usernameField}) => {
   const loginMethods = app.get('loginMethods') ? app.get('loginMethods') : [];
   app.set('loginMethods',
     loginMethods.concat({
-      url : '/html/localsignin.html',
-      text : 'Log in locally'
+      url: '/html/localsignin.html',
+      text: 'Log in locally'
     }));
 };
